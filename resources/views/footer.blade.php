29cb480a20f8efa81e8c03b49d7b6d74431a942d
Roaa Alhakimi <58307502+ro5a@users.noreply.github.com>
2022-04-04 12:23:36 +0300

    include header and footer

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,30 @@
+<div class="flex1">
+  <div><h4>job location</h4>
+  <ul><li>sololearn</li>
+    <li>google</li>
+    <li>facebook</li>
+  </ul></div>
+  <div class="text"><h4 style="color: #FFC312;">Jobs</h4>
+  <p class="text-wrap">this the place where you find your job, you wanted.</p><div>
+    <i class="fa fa-facebook"></i>
+    <i class="fa fa-twitter"></i>
+    <i class="fa fa-pinterest"></i>
+  </div>
+</div>
+  <div><h4>Hot links</h4>
+  <ul>
+    <li>about</li>
+    <li>blog</li>
+    <li>contuct</li>
+  </ul> </div>
+  
+</div>
+<div class="copy"><p>all reverse Jobs company &copy copyright 2022</p></div>
+    </footer>
+    <script src="js/app.js">
+    
+    </script>
+     <script src="https://unpkg.com/boxicons@2.1.1/dist/boxicons.js"></script>
+
+</body>
+</html>
