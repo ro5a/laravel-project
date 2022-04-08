7b150ccf16f9569601b560bfff69b2c82d15539a
Roaa Alhakimi <58307502+ro5a@users.noreply.github.com>
2022-04-08 15:50:42 +0300

    validate login

--- app/Http/Controllers/admin/AuthController.php
+++ app/Http/Controllers/admin/AuthController.php
@@ -4,12 +4,28 @@ namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
+   
     public function showLogin(){
         return view('admin.login');
     }
+    public function login(Request $req){
+        Validator::validate(); 
+
+    }
+    public function register(){
+
+    }
+    public function resetPassword(){
+
+    }
+    public function logout(){
+
+    }
+
     public function create(){
         return view('admin.users.add_user');
     }
